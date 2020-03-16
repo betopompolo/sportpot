44b0c2b29c713c2cf8d3d6d259a6279a2cd49ce8
betopompolo <beto.nassu@gmail.com>
2020-03-16 11:39:31 -0300

    Initial commit

---
+++ src/main/java/alura/sportpot/SportpotApplication.java
@@ -0,0 +1,13 @@
+package alura.sportpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportpotApplication.class, args);
+	}
+
+}
